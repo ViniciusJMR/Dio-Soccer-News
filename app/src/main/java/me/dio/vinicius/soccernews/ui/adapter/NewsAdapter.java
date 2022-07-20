336565804e011928bc2ec6435d9bd4c2bb609990
Vinicius Rodrigues <viniciusrodrigues@pop-os.localdomain>
2022-07-19 22:06:27 -0300

    Added onClickListener to 'OPEN LINK' button

--- app/src/main/java/me/dio/vinicius/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/me/dio/vinicius/soccernews/ui/adapter/NewsAdapter.java
@@ -1,5 +1,7 @@
 package me.dio.vinicius.soccernews.ui.adapter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,6 +50,11 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         Picasso.get().load(news.getImage())
                 .fit()
                 .into(holder.binding.ivNews);
+        holder.binding.btOpenLink.setOnClickListener(view -> {
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(news.getLink()));
+            holder.itemView.getContext().startActivity(i);
+        });
 
     }
 
